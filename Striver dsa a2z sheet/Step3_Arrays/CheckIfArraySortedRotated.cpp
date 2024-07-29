f6377ad5454425c9209c39c59716643d6afe12ef
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2024-07-29 14:24:40 +0530

    Update CheckIfArraySortedRotated.cpp

--- Striver dsa a2z sheet/Step3_Arrays/CheckIfArraySortedRotated.cpp
+++ Striver dsa a2z sheet/Step3_Arrays/CheckIfArraySortedRotated.cpp
@@ -1,1 +1,27 @@
+// https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/#:~:text=Input%3A%20nums%20%3D%20%5B2%2C,no%20rotation)%20to%20make%20nums.
+/* Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+There may be duplicates in the original array.
+Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+Example 1:
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+*/
 
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int flag = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) flag++;
+        }
+        
+        if (nums[nums.size() - 1] > nums[0]) 
+            flag++;
+
+        return flag <= 1;
+    }
+    
+};
